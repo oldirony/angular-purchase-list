2e36cf115d4154daa2f96510bc27078f75adb97b
oldirony <raffaele.abramini@gmail.com>
2017-09-29 15:12:48 +0100

    Recipe list
    - create list of Recipe
    - style content

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RecipeModel } from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: RecipeModel[] = [
+      new RecipeModel('Lasagna', 'How to make delicious lasagnas', 'https://assets.bonappetit.com/photos/57adf3c353e63daf11a4dfa2/16:9/w_1000,c_limit/lasagna-bolognese.jpg')
+  ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  createNewRecipe() {
+    this.recipes.push(new RecipeModel('123','2','2'));
   }
 
 }
